c1e7c353d84665a919e769ff210bbae8fc4158e6
Christophe Biguereau <christophe.biguereau@canal-plus.com>
2022-03-10 22:07:23 +0100

    feat: Create bash module to compare 2 versions.

--- compare_versions.sh
+++ compare_versions.sh
@@ -1,95 +1,111 @@
 #!/bin/bash
 
-# Compare 2 versions and echo:
-# * 0 if same version.
-# * 1 if v1 is newer than v2.
-# * -1 if v1 is older than v2.
-function compare(){
+# Fill small string version length with 0
+# param: ver_1: First version value.
+# param: ver_2: Second version value.
+function __fillVersionLength(){
+    # Keep IFS before variable assignment.
+    # It's use to split version string as array.
+    local IFS="."
+    #shellcheck disable=SC2206
+    local -a L_VERSION1_ARR=(${1})
+    #shellcheck disable=SC2206
+    local -a L_VERSION2_ARR=(${2})
 
-	local v1="${1}"
-	local v2="${2}"
+    # If ver_1 length is smaller than ver_2
+    if [ "${#L_VERSION1_ARR[@]}" -lt "${#L_VERSION2_ARR[@]}" ];
+    then
+        # fill ver_1 with 0
+        for ((i=${#L_VERSION1_ARR[@]}; i < ${#L_VERSION2_ARR[@]}; i++));
+        do
+            L_VERSION1_ARR["${i}"]=0
+        done
 
-	# check if versions are the same.
-	if [[ ${v1} == ${v2} ]];
-	then
-		return 0
-	fi
+    elif [ "${#L_VERSION1_ARR[@]}" -gt "${#L_VERSION2_ARR[@]}" ];
+    then
+        # fill ver_1 with 0
+        for ((i=${#L_VERSION2_ARR[@]}; i < ${#L_VERSION1_ARR[@]}; i++));
+        do
+            L_VERSION2_ARR["${i}"]=0
+        done
+    fi
 
-	# Convert versions in list
-	IFS='.' read -r -a v1 <<< "${v1}"
-	IFS='.' read -r -a v2 <<< "${v2}"
-
-	# Fill empty field in v1 if v2 size
-	# is longer than v2
-	for ((i=${#v1[@]}; i<${#v2[@]}; i++ ))
-	do
-		v1[${i}]=0
-	done
-
-	# compare each v1 fields to v2
-	for ((i=0; i<${#v1[@]}; i++))
-    	do
-		# Catch if idx value doesn't exist in v2
-        	if [[ -z ${v2[i]} ]]
-        	then
-            		v2[i]=0
-        	fi
-
-		# if v1 is newer than v2
-        	if ((10#${v1[i]} > 10#${v2[i]}))
-        	then
-            		return 1
-        	fi
-
-		# if v1 is older than v2
-        	if ((10#${v1[i]} < 10#${v2[i]}))
-        	then
-            		return 2
-        	fi
-    	done
-
-    	return 0
+    # Return versions with same length.
+    echo -e "${L_VERSION1_ARR[*]// /.} ${L_VERSION2_ARR[*]// /.}"
 }
 
-# Check if version is newer
-function is_newer(){
-	local v1="${1}"
-	local v2="${2}"
+# Verify than both version are equals
+# param version1: First version to compare.
+# param version2: Second version to compare.
+function versionsAreEquals(){
+    local L_VERSION1="${1}"
+    local L_VERSION2="${2}"
 
-	compare ${v1} ${v2}
+    # Fill version string to have same length
+    IFS=" " read -r L_VERSION1 L_VERSION2 <<< "$(__fillVersionLength "${L_VERSION1}" "${L_VERSION2}")"
 
-	if compare ${v1} ${v2};
-	then
-		return 0
-	else
-		return 1
-	fi
+    # Check if versions are equal
+    if [ "${L_VERSION1}" == "${L_VERSION2}" ];
+    then
+        return 0
+    else
+        return 1
+    fi
 }
 
-# Check if version is newer
-function is_older(){
-	local v1="${1}"
-	local v2="${2}"
+# Verify than version are newer than another version
+# param version1: First version to compare.
+# param version2: Second version to compare.
+function versionIsNewer(){
+    local L_VERSION1="${1}"
+    local L_VERSION2="${2}"
 
-	compare ${v1} ${v2}
+    # Fill version string to have same length
+    IFS=" " read -r L_VERSION1 L_VERSION2 <<< "$(__fillVersionLength "${L_VERSION1}" "${L_VERSION2}")"
 
-	if [[ $? == 2 ]];
-	then
-		return 0
-	else
-		return 1
-	fi
+    # Convert versions string to array
+    IFS="." read -ra L_VERSION1_ARR <<< "${L_VERSION1}"
+    IFS="." read -ra L_VERSION2_ARR <<< "${L_VERSION2}"
+
+    for ((i=0; i < ${#L_VERSION1_ARR[@]}; i++));
+    do
+        if (( L_VERSION1_ARR[i] > L_VERSION2_ARR[i] ));
+        then
+            return 0
+        elif (( L_VERSION1_ARR[i] < L_VERSION2_ARR[i] ));
+        then
+            return 1
+        fi
+    done
+
+    return 1
 }
 
-# Check if versions are the same
-function is_equal(){
-	local v1="${1}"
-	local v2="${2}"
 
-	if compare ${v1} ${v2};
-	then
-		return 0
-	else
-		return 1
-	fi
+# Verify than version are older than another version
+# param version1: First version to compare.
+# param version2: Second version to compare.
+function versionIsOlder(){
+    local L_VERSION1=${1}
+    local L_VERSION2=${2}
+
+    # Fill version string to have same length
+    IFS=" " read -r L_VERSION1 L_VERSION2 <<< "$(__fillVersionLength "${L_VERSION1}" "${L_VERSION2}")"
+
+    # Convert versions string to array
+    IFS="." read -ra L_VERSION1_ARR <<< "${L_VERSION1}"
+    IFS="." read -ra L_VERSION2_ARR <<< "${L_VERSION2}"
+
+    for ((i=0; i < ${#L_VERSION1_ARR[@]}; i++));
+    do
+        if (( L_VERSION1_ARR[i] < L_VERSION2_ARR[i] ));
+        then
+            return 0
+        elif (( L_VERSION1_ARR[i] > L_VERSION2_ARR[i] ));
+        then
+            return 1
+        fi
+    done
+
+    return 1
 }
